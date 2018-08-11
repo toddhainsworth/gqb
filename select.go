172c22af54ad0d9c294db9ed91b3d2f9a694021d
Todd Hainsworth <hainsworth.todd@gmail.com>
2018-08-11 21:38:00 +0930

    extremely WIP select query

--- select.go
+++ select.go
@@ -1,11 +1,33 @@
 package gqb
 
+import (
+	"strings"
+	"fmt"
+)
+
 // SelectQuery defines a query that selects data from the database
 type SelectQuery struct {
+	Table      string
 	Columns    []string
 	Conditions map[string]map[string]string
 }
 
+// NewSelectQuery creates a SelectQuery with the given columns
+func NewSelectQuery(table string, columns []string) SelectQuery {
+	return SelectQuery { Table: table, Columns: columns }
+}
+
+// ToString returns the query as a usable string
 func (s SelectQuery) ToString() string {
-	return "" // todo: construct from parts
+	return fmt.Sprintf("SELECT %s FROM %s;", s.getColumns(), s.Table)
+}
+
+func (s SelectQuery) getColumns() (columns string) {
+	columns = "*"
+
+	if len(s.Columns) > 0 {
+		columns = strings.Join(s.Columns, ",")
+	}
+
+	return
 }
--- select_test.go
+++ select_test.go
@@ -2,10 +2,25 @@ package gqb
 
 import "testing"
 
-func TestToString(t *testing.T) {
-	query := SelectQuery{}
+var query = NewSelectQuery("person", []string{"name"})
+var allQuery = NewSelectQuery("person", []string{})
 
-	if query.ToString() != "" {
-		t.Errorf("Expected query to equal \"\" but got \"%s\"", query)
+func TestNewSelectQuery(t *testing.T) {
+	if column := query.Columns[0]; column != "name" {
+		t.Errorf("Expected first column to equal \"name\" but got \"%s\"", column)
+	}
+}
+
+func TestToString(t *testing.T) {
+	expected := "SELECT name FROM person;"
+
+	if str := query.ToString(); str != expected {
+		t.Errorf("Expected query to equal \"%s\" but got \"%s\"", expected, str)
+	}
+
+	expected = "SELECT * FROM person;"
+
+	if str := allQuery.ToString(); str != expected {
+		t.Errorf("Expected query to equal \"%s\" but got \"%s\"", expected, str)
 	}
 }
